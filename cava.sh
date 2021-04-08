059acb3a284e17aeee7b600e0ee77dc4a11f505a
ray-pH <rayhan.azizi9@gmail.com>
2021-04-09 00:17:38 +0700

    files

--- cava.sh
+++ cava.sh
@@ -1,1 +1,25 @@
-/home/ray/.config/polybar/cava.sh
+#! /bin/bash
+
+bar="▁▂▃▄▅▆▇█"
+dict="s/;//g;"
+
+# creating "dictionary" to replace char with bar
+i=0
+while [ $i -lt ${#bar} ]
+do
+    dict="${dict}s/$i/${bar:$i:1}/g;"
+    i=$((i=i+1))
+done
+
+# make sure to clean pipe
+pipe="/tmp/cava.fifo"
+if [ -f $pipe ]; then
+    unlink $pipe
+fi
+# run cava in the background
+cava -p $HOME/.config/polybar/cava_raw_config &
+
+# reading data from fifo
+while read -r cmd; do
+    echo $cmd | sed $dict
+done <"$pipe"
